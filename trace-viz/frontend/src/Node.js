1cda57a3aac5068cebd934075806e83b6f135c29
Alan Vezina <alan.vezina@modusbox.com>
2017-10-09 18:06:06 -0700

    Add dashed option to Node class
    - Making a cloud icon was too difficult to do right now, so instead we will make the cloud icons dashed versions of the node icons.
    - Added better text positioning for one line text nodes.

--- trace-viz/frontend/src/Node.js
+++ trace-viz/frontend/src/Node.js
@@ -3,15 +3,25 @@ import { NODE_WIDTH } from './constants';
 
 class Node extends PureComponent {
   render() {
-    const { x, y, height, name, text, rotate } = this.props;
-
-    // This wonderfully magic incantation ensures that our text is centered vertically for the most part.
-    // It's awful because SVG is awful.
-    const textY = (height / 2) - (Math.max(24, 17 * (text.length - 1)));
+    const { x, y, height, name, text, rotate, dashed } = this.props;
+    let textY;
     let rotateStr = '';
+    let sda = null;
+
+    if (text.length > 1) {
+      // This wonderfully magic incantation ensures that our text is centered vertically for the most part.
+      // It's awful because SVG is awful.
+      textY = (height / 2) - (Math.max(24, 17 * (text.length - 1)));
+    } else {
+      textY = (height / 2) - 15;
+    }
 
     if (rotate) {
       rotateStr += `rotate(${rotate} ${NODE_WIDTH / 2} ${height / 2})`;
+    }
+
+    if (dashed) {
+      sda = '5, 5';
     }
 
     return (
@@ -26,6 +36,7 @@ class Node extends PureComponent {
           rx="8"
           ry="8"
           transform={rotateStr}
+          strokeDasharray={sda}
         />
         <text textAnchor="middle" y={textY}>
           {text.map((t, i) => <tspan key={i} x={NODE_WIDTH / 2} dy="20">{t}</tspan>)}
