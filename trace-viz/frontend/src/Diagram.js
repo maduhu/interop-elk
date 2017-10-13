c8e4d3965d6cc9b6fd945654ecd3b43aec365d24
Alan Vezina <alan.vezina@modusbox.com>
2017-10-12 18:10:55 -0700

    First pass at querying ES for traceData
    - right now we check data for various "actions" and build an animation sequence out of that.
    - TODO: render animation sequence.
    - TODO: allow user to select traceID to render

--- trace-viz/frontend/src/Diagram.js
+++ trace-viz/frontend/src/Diagram.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import DiagramControls from './DiagramControls';
-import PayerDiagram from './PayerDiagram';
-import CentralDiagram from './CentralDiagram';
-import PayeeDiagram from './PayeeDiagram';
-import SectionBackgrounds from './SectionBackgrounds';
-import TraceTable from './TraceTable';
+import SimplifiedDiagram from './SimplifiedDiagram';
+import { has } from './utils';
 import * as d3 from 'd3';
-import ExampleResponse from './example_response.json';
+import { Client } from 'elasticsearch';
 import {
-  DFSP_WIDTH,
-  CENTRAL_WIDTH,
-  SECTION_MARGIN,
   FILL,
   STROKE,
   STROKE_START,
@@ -19,6 +13,101 @@ import {
   FILL_END,
 } from './constants';
 import './Diagram.css';
+
+/* eslint-disable */
+export const colorMap = {
+  lookup: '.color--lookup',
+  resolve: '.color--resolve',
+  quoteFees: '.color--quote',
+  quoteRoute: '.color--quote',
+  prepare: '.color--prepare',
+  fulfill: '.color--fulfill',
+};
+const animationSequences = {
+  lookup: [
+    // These should be highlighted purple
+    'payer-dfsp-logic',
+    'payer-dfsp-logic-dot',
+    'payer-scheme-adapter',
+    'lookup-identifier',
+    'central-directory',
+    'pathfinder-dot',
+    'pathfinder',
+  ],
+  resolve: [
+    // These should be highlighted purple
+    'payer-dfsp-logic',
+    'payer-dfsp-logic-dot',
+    'payer-scheme-adapter',
+    'resolve-recipient',
+    'payee-scheme-adapter',
+    'payee-dfsp-logic-dot',
+    'payee-dfsp-logic',
+  ],
+  quoteFees: [
+    // These should be highlighted orange
+    'payer-dfsp-logic',
+    'payer-dfsp-logic-dot',
+    'payer-scheme-adapter',
+    'quote-fees',
+    'payee-scheme-adapter',
+    'payee-dfsp-logic-dot',
+    'payee-dfsp-logic',
+    'create-condition',
+  ],
+  quoteRoute: [
+    // These should be highlighted orange
+    'payer-dfsp-logic',
+    'payer-dfsp-logic-dot',
+    'payer-scheme-adapter',
+    'payer-dfsp-connector',
+    'payer-quote-route',
+    'central-ist',
+    'payee-quote-route',
+    'payee-dfsp-connector',
+    'payee-scheme-adapter',
+    'payee-dfsp-logic-dot',
+    'payee-dfsp-logic',
+  ],
+  prepare: [
+    // These should be highlighted blue
+    'payer-dfsp-logic',
+    'payer-dfsp-logic-dot',
+    'payer-scheme-adapter',
+    'payer-dfsp-connector',
+    'payer-ledger-adapter-dot',
+    'payer-ledger-adapter',
+    'payer-ledger-db-dot',
+    'payer-ledger-db',
+    'payer-prepare',
+    'central-ist',
+    'central-ledger-db-dot',
+    'central-ledger-db',
+    'payee-prepare',
+    'payee-dfsp-connector',
+    'payee-dfsp-ledger-adapter-dot',
+    'payee-dfsp-ledger-adapter',
+    'evaluate-condition'
+  ],
+  fulfill: [
+    // These should be highlighted green
+    'evaluate-condition',
+    'payee-dfsp-connector',
+    'payee-notify-fulfillment',
+    'central-ist',
+    'central-ledger-db-dot',
+    'central-ledger-db',
+    ['payer-notify-fulfillment', 'payee-notify-fulfillment'],
+    ['payer-dfsp-connector', 'payee-dfsp-connector'],
+    ['payer-ledger-adapter', 'payee-ledger-adapter'],
+    ['payer-ledger-db-dot', 'payee-ledger-db-dot'],
+    ['payer-ledger-db', 'payee-ledger-db'],
+    ['payer-scheme-adapter', 'payee-scheme-adapter'],
+    ['payer-dfsp-logic-dot', 'payee-dfsp-logic-dot'],
+    ['payer-dfsp-logic', 'payee-dfsp-logic'],
+  ],
+};
+/* eslint-enable */
 
 function animateEdge(node, action, duration, strokeStart) {
   const sel = d3.select(`.line--${node}-${action} path`);
@@ -60,25 +149,13 @@ function animateNodeAndEdge(node, action, duration, strokeStart, strokeEnd, fill
   animateEdge(node, action, duration, strokeStart, strokeEnd);
 }
 
-function processData(data) {
-  /**
-   * processData extracts the information we need from an ElasticSearch query and converts it to a less verbose format.
-   */
-  return data.hits.hits.map((hit) => {
-    const source = hit._source; // eslint-disable-line no-underscore-dangle
-
-    return {
-      timestamp: new Date(source['@timestamp']),
-      environment: source.l1p_environment,
-      service: source.l1p_service_id,
-      message: source.message,
-    };
-  });
-}
-
 class Diagram extends Component {
   constructor(props) {
     super(props);
+    this.parseTraceIds = this.parseTraceIds.bind(this);
+    this.getTraceIds = this.getTraceIds.bind(this);
+    this.parseTraceData = this.parseTraceData.bind(this);
+    this.getTraceData = this.getTraceData.bind(this);
     this.startPlayLoop = this.startPlayLoop.bind(this);
     this.stopPlayLoop = this.stopPlayLoop.bind(this);
     this.playPause = this.playPause.bind(this);
@@ -87,27 +164,21 @@ class Diagram extends Component {
     this.stop = this.stop.bind(this);
     this.reset = this.reset.bind(this);
     this.selectStep = this.selectStep.bind(this);
-
-    const fakePath = [
-      ['payer-dfsp-logic', 'quote'],
-      ['payer-interop-scheme-adapter', 'quote-route'],
-      ['payer-ilp-service', 'quote-route'],
-      ['payer-ilp-client', 'quote-route'],
-      ['payer-ilp-connector', 'quote-route'],
-    ];
-    const fakeData = processData(ExampleResponse).slice(0, fakePath.length);
-
+    this.client = new Client({ host: 'localhost:9200' });
+    this.getTraceIds();
     this.state = {
+      traceId: null, // Good test id: 5e97e69e-93af-4b13-acdd-a60bb790a3bf
+      traceIds: null,
+      traceSequence: null,
       step: null,
+      stepData: {},
       loopId: null,
-      data: fakeData, // TODO: actually query the ES server for data.
-      path: fakePath, // TODO: generate path based on data retrieved from ElasticSearch
     };
   }
 
   componentDidMount() {
     if (this.state.path !== null) {
-      this.playPause();
+      // this.playPause();
     }
   }
 
@@ -116,6 +187,132 @@ class Diagram extends Component {
       // TraceId changed, stop all animations, reset state, load new data, then start playing
       this.reset();
     }
+  }
+
+  parseTraceIds(data) {
+    let loadTraceData = false;
+
+    this.setState((state) => {
+      const newState = {};
+      const traceIdMap = {};
+      const traceIdList = [];
+
+      data.hits.hits.forEach((hit) => {
+        const source = hit._source; // eslint-disable-line no-underscore-dangle
+        const traceId = source.l1p_trace_id;
+
+        if (!has.call(traceIdMap, traceId)) {
+          // The trace data is sorted in ascending order so we can assume the first traceId we see is the newest one.
+          traceIdMap[traceId] = true;
+          traceIdList.push(traceId);
+        }
+      });
+
+      newState.traceIds = traceIdList;
+
+      if (state.traceId === null) {
+        newState.traceId = traceIdList[0];
+        loadTraceData = true;
+      }
+
+      return newState;
+    }, () => {
+      if (loadTraceData) {
+        this.getTraceData();
+      }
+    });
+  }
+
+  getTraceIds() {
+    this.client.search({
+      index: 'l1p_index_*',
+      body: {
+        size: 50,
+        sort: {
+          '@timestamp': { order: 'desc' },
+        },
+        query: {
+          bool: {
+            // We are only interested in data that has a traceId and a callType associated.
+            must: [{
+              exists: {
+                field: 'l1p_trace_id',
+              },
+            }, {
+              exists: {
+                field: 'l1p_call_type',
+              },
+            }],
+          },
+        },
+      },
+    }).then(this.parseTraceIds);
+  }
+
+  parseTraceData(data) {
+    this.setState(() => {
+      const newState = { data: data.hits.hits };
+      const callTypes = {};
+      const sequences = [];
+
+      data.hits.hits.forEach((hit) => {
+        const source = hit._source; // eslint-disable-line
+
+        if (has.call(source, 'l1p_call_type')) {
+          const callType = source.l1p_call_type;
+
+          if (!has.call(callTypes, callType)) {
+            callTypes[callType] = {};
+          }
+
+          callTypes[callType][`${source.l1p_environment} - ${source.l1p_service_id}`] = true; // eslint-disable-line
+        }
+      });
+
+      if (has.call(callTypes, 'user-lookup')) {
+        sequences.push('lookup');
+      }
+
+      if (has.call(callTypes, 'payee-details')) {
+        sequences.push('resolve');
+      }
+
+      if (has.call(callTypes, 'quote-fees')) {
+        sequences.push('quoteFees');
+      }
+
+      if (has.call(callTypes, 'quote-route')) {
+        sequences.push('quoteRoute');
+      }
+
+      if (has.call(callTypes, 'rest-prepare') || has.call(callTypes, 'dfsp-prepare')) {
+        sequences.push('prepare');
+      }
+
+      if (has.call(callTypes, 'rest-fulfill') || has.call(callTypes, 'dfsp-fulfill')) {
+        sequences.push('fulfill');
+      }
+
+      newState.traceSequence = sequences;
+      return newState;
+    });
+  }
+
+  getTraceData() {
+    this.client.search({
+      index: 'l1p_index_*',
+      body: {
+        size: 1500,
+        sort: {
+          '@timestamp': { order: 'asc' },
+        },
+        query: {
+          query_string: {
+            query: `l1p_trace_id:"${this.state.traceId}"`,
+          },
+        },
+      },
+    }).then(this.parseTraceData);
   }
 
   startPlayLoop() {
@@ -231,49 +428,16 @@ class Diagram extends Component {
   }
 
   render() {
-    const width = (DFSP_WIDTH * 2) + CENTRAL_WIDTH + (SECTION_MARGIN * 2);
-    const height = 680;
+    const width = 1024;
+    const height = 500;
     const zoom = this.props.zoom ? this.props.zoom : 1;
     const isPlaying = this.state.loopId !== null;
 
     return (
       <div className="architecture-diagram">
         <svg className="diagram-canvas" width={width} height={height}>
-          <defs>
-            <marker
-              id="arrow"
-              markerWidth="8"
-              markerHeight="8"
-              refX="8"
-              refY="3"
-              orient="auto"
-              markerUnits="strokeWidth"
-            >
-              <path d="M0,0 L0,6 L9,3 z" fill="#000" />
-            </marker>
-
-            <marker
-              id="arrow--selected"
-              markerWidth="8"
-              markerHeight="8"
-              refX="8"
-              refY="3"
-              orient="auto"
-              markerUnits="strokeWidth"
-            >
-              <path d="M0,0 L0,6 L9,3 z" fill="#060" />
-            </marker>
-          </defs>
-
           <g className="diagram-zoom-container" transform={`scale(${zoom})`}>
-            {/* Render the section backgrounds first so everything is rendered on top of them */}
-            <SectionBackgrounds />
-
-            <PayerDiagram />
-
-            <CentralDiagram />
-
-            <PayeeDiagram />
+            <SimplifiedDiagram />
           </g>
         </svg>
 
@@ -285,8 +449,6 @@ class Diagram extends Component {
           stop={this.stop}
           forward={this.forward}
         />
-
-        <TraceTable step={this.state.step} path={this.state.path} data={this.state.data} selectStep={this.selectStep} />
       </div>
     );
   }
