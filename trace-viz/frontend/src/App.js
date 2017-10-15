420badaebccd1348b37347abf46b8dd1bf532044
Alan Vezina <alan.vezina@modusbox.com>
2017-10-14 18:34:42 -0700

    Simplify App.js

--- trace-viz/frontend/src/App.js
+++ trace-viz/frontend/src/App.js
@@ -1,33 +1,12 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Diagram from './Diagram';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.updateTraceId = this.updateTraceId.bind(this);
-    this.loadTraceData = this.loadTraceData.bind(this);
-
-    this.state = {
-      traceId: '',
-      traceData: null,
-    };
-  }
-
-  updateTraceId(traceId) {
-    this.setState({ traceId }, this.loadTraceData);
-  }
-
-  loadTraceData() {
-    console.log(`Loading trace data for ${this.state.traceId}`);
-  }
-
+class App extends PureComponent {
   render() {
     return (
       <div className="trace-viz-app">
-        {/*<Header traceId={this.state.traceId} updateTraceId={this.updateTraceId} />*/}
-        <Diagram traceId={this.state.traceId} data={this.state.traceData} />
-        {/*<Positions />*/}
+        <Diagram />
       </div>
     );
   }
